ff931609227649f452dbb02b7821261aabd13d80
Ethereal Iska <etherealiska@gmail.com>
2023-08-28 16:51:24 -0700

    use jsonrpcprovider

--- web3modal-wagmi-react/src/pages/_app.jsx
+++ web3modal-wagmi-react/src/pages/_app.jsx
@@ -3,6 +3,7 @@ import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { mainnet, optimism, polygon } from "wagmi/chains";
 import { getDefaultWallets, connectorsForWallets, RainbowKitProvider, darkTheme } from '@rubicondefi/rainbowkit';
 import { publicProvider } from 'wagmi/providers/public';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 import { braveWallet } from '@rubicondefi/rainbowkit/wallets';
 
 import "../styles.css";
@@ -15,10 +16,36 @@ if (!process.env.NEXT_PUBLIC_PROJECT_ID) {
 }
 const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 
+const network = {
+  MAINNET: 1,
+  ROPSTEN: 3,
+  RINKEBY: 4,
+  GOERLI: 5,
+  KOVAN: 42,
+  OPTIMISM_KOVAN: 69,
+  OPTIMISM_MAINNET: 10,
+  POLYGON_MAINNET: 137,
+  POLYGON_MUMBAI: 80001,
+  BSC_MAINNET: 56,
+  GNOSIS_CHAIN_MAINNET: 100,
+  FANTOM_OPERA_MAINNET: 250,
+  ARBITRUM_MAINNET: 42161,
+  ARBITRUM_GOERLI: 421613,
+  BASE_MAINNET: 8453,
+  BASE_GOERLI: 84531,
+  AVALANCHE_C_CHAIN_MAINNET: 43114,
+  AURORA_MAINNET: 1313161554,
+  OPTIMISM_GOERLI: 420,
+}
+
+const RPC_URLS = {
+  [network.OPTIMISM_MAINNET]: "https://rpc.ankr.com/optimism"
+}
+
 // 2. Configure wagmi client
 const { chains, publicClient } = configureChains(
   [optimism],
-  [publicProvider()]
+  [jsonRpcProvider({ rpc: (chain) => ({ http: RPC_URLS[chain.id] }) })]
 );
 
 const { wallets } = getDefaultWallets({
